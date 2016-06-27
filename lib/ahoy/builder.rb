45bc975ca355af69a8b988aee107ee1bd92dbc66
jvillarejo <arzivian87@gmail.com>
2016-06-27 16:24:04 -0300

    refactored

--- lib/ahoy/builder.rb
+++ lib/ahoy/builder.rb
@@ -1,8 +1,6 @@
 module Ahoy
   module Controller
     module Builder
-      include Ahoy::StringGenerator
-
       def build_visit
         params[:visit_token] ||= generate_token
         params[:visitor_token] ||= generate_token
@@ -16,6 +14,10 @@ module Ahoy
           v.landing_page = params[:landing_page] || request.url if v.respond_to?(:landing_page=)
           v.user = current_user if respond_to?(:current_user) and v.respond_to?(:user=)
         end
+      end
+
+      def generate_token
+        SecureRandom.uuid
       end
     end
   end
--- lib/ahoy/string_generator.rb
+++
@@ -1,7 +0,0 @@
-module Ahoy
-  module StringGenerator
-    def generate_token(length=32)
-      SecureRandom.uuid
-    end
-  end
-end
